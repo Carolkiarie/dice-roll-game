c8c217594050a848a470487f9d325bbb13da3863
carolkiarie <wairimucarolk@gmail.com>
2023-03-21 13:09:09 +0100

    all added

---
+++ README.md
@@ -0,0 +1,19 @@
+
+Website: 
+This game dice-roll-game has two players. It allows the players to cumulate their scores and the player who gets to 100. The player wins.
+
+To implement the game there were certain logics that had to be considered.
+
+1. The Roll dice button. (
+    1. generating a random number. then display the dice images according to the random numbers.
+    check if the number is 1 and if it is set the current score to zero. But if it is any other number other than 1 then take the random number and add it to the current score.
+    2. Switch the players if a user rolls a number one
+    and also set the current score to 0
+)
+2.The hold button(
+    Check if a player has a total of 100 so as to declare them a winner.
+    store the current score so that when they roll the dice the previous score and the new score will be added together.
+)
+3.New game button(
+    Reset the scores of the game, restore the classes to initial state
+)
---
+++ sandbox.js
@@ -0,0 +1,68 @@
+const score0El = document.getElementById('score--0')
+const score1El= document.getElementById('score--1')
+const diceEl = document.querySelector('.dice')
+const diceRoll = document.querySelector('.btn--roll')
+const newGame = document.querySelector('.btn--new')
+const holdGame = document.querySelector('.btn--hold')
+const current0El = document.getElementById('current--0')
+const current1El = document.getElementById('current--1')
+const player0EL= document.querySelector('.player--0')
+const player1EL= document.querySelector('.player--1')
+let currentScore = 0
+let activePlayer = 0
+let scores = [0, 0]
+let playing = true
+score0El.textContent= 0
+score1El.textContent= 0
+const switchPlayer = ()=>{
+    document.getElementById(`current--${activePlayer}`).textContent= 0
+    activePlayer= activePlayer ===0 ? 1: 0
+    currentScore= 0
+    
+ player0EL.classList.toggle('player--active')
+ player1EL.classList.toggle('player--active')
+}
+diceRoll.addEventListener('click', ()=>{
+    if(playing){
+        let number = Math.trunc(Math.random() *6) +1;
+    diceEl.style.display= ('block')
+    diceEl.src= `/assets/dice-${number}.png`
+    if(number !== 1){
+        currentScore= currentScore + number
+       document.getElementById(`current--${activePlayer}`).textContent= currentScore
+    }else{
+        switchPlayer()
+    }
+    }
+   
+})
+holdGame.addEventListener('click', function() {
+    if(playing)
+    {
+        scores[activePlayer]= currentScore + scores[activePlayer]
+        document.getElementById(`score--${activePlayer}`).textContent= scores[activePlayer]
+        if(scores[activePlayer] >= 100){
+            playing= false
+            diceEl.style.display= ('none')
+            document.querySelector(`.player--${activePlayer}`).classList.add('player--winner')
+            document.querySelector(`.player--${activePlayer}`).classList.remove('player--active')
+            
+        }else{
+            switchPlayer()
+        }}
+
+    
+})
+newGame.addEventListener('click', function(){
+    score0El.textContent=0 
+ score1El.textContent= 0
+ current0El.textContent= 0
+ current1El.textContent= 0
+ currentScore= 0
+ scores= [0,0]
+ activePlayer= 0
+    playing= true
+ document.querySelector(`.player--${activePlayer}`).classList.remove('player--winner')
+ document.querySelector(`.player--${activePlayer}`).classList.add('player--active')
+ 
+})
